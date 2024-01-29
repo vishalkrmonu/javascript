624a575c3ebf5739352ffc59c3c7728327f43f41
Vishal Kumar <150379111+vishalkrmonu@users.noreply.github.com>
2024-01-29 07:31:00 +0000

    compare

--- javascript/tut1.js
+++ javascript/tut1.js
@@ -123,3 +123,27 @@ console.log(gameCounter);
 ++gameCounter;
 console.log(gameCounter);
 // https://tc39.es/ecma262/multipage/abstract-operations
+
+
+
+//day8:comparision
+console.log("----day8-----");
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2" >1);
+// console.log("02" >1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log("2" ===2);
