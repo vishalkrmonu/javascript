099358bf9bf8d72663d3470d1bf48755f72e65fd
Vishal Kumar <150379111+vishalkrmonu@users.noreply.github.com>
2024-01-29 15:50:57 +0000

    String

--- javascript/3_String.js
+++ javascript/3_String.js
@@ -9,7 +9,7 @@ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 const gameName =new String('vishal-kr') //type of string dekho console me isko likhkr
 
 console.log(gameName[0]);
-console.log(gameName.__proto__);
+console.log(gameName.__proto__); 
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
